5f41034968fbda0c6eb91331f8fde7a3cf96b75f
x8o <shigeru@rim-inc.org>
2022-06-21 15:28:19 +0900

    Create test_import.py

---
+++ test_import.py
@@ -0,0 +1,16 @@
+import torch 
+from PIL import Image
+import os
+import dlib
+import collections
+from typing import Union, List
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import PIL.Image
+import PIL.ImageFile
+import scipy.ndimage
+import requests
+
+a = 1
+print(a)
